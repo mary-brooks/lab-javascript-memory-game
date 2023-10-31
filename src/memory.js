237c89c4171bacd6a146f848e204fc53fd2382c9
mary-brooks <maryemmabrooks@gmail.com>
2023-10-31 16:32:04 +0000

    lab completed

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,47 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
+    this.shuffleCards();
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[randomIndex]] = [
+        this.cards[randomIndex],
+        this.cards[i],
+      ];
+    }
+
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    const totalPairs = this.cards.length / 2;
+
+    if (this.pairsGuessed === totalPairs) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
